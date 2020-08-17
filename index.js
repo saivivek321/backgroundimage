b409b7b2ee23b114cca834a753453103e5340012
saivivek321 <63186987+saivivek321@users.noreply.github.com>
2020-08-17 14:28:37 +0530

    background

---
+++ index.js
@@ -0,0 +1,5 @@
+function setbackground(element){
+
+	document.getElementById("image").setAttribute("style","background-image:url("+element.src+");background-size:100%;background-repeat:no-repeat;");
+
+}
